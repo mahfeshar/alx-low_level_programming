6a4ab6b49ec781a0a1cb8eeb53aef8676238627d
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-10 07:00:56 -0700

    edit mandatory tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	prinf("Size of a char: %u byte(s)", sizeof(char));
-	prinf("Size of an int: %u byte(s)", sizeof(int));
-	prinf("Size of a long int: %u byte(s)", sizeof(long int));
-	prinf("Size of a long long int: %u byte(s)", sizeof(long long int));
-	prinf("Size of a float: %u byte(s)", sizeof(float));
+	prinf("Size of a char: %u byte(s)\n", sizeof(char));
+	prinf("Size of an int: %u byte(s)\n", sizeof(int));
+	prinf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	prinf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	prinf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
