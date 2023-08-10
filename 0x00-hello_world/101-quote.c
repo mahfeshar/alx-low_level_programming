af57e9dfa33facdbc3887ea62a6c49b91cdbf77f
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-10 10:03:27 -0700

    Edit again in 2nd advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int n = sizeof(q) / sizeof(q[0]);
+	/*int n = sizeof(q) / sizeof(q[0]);*/
 
-	write(2, q, n);
+	write(2, q, sizeof(q) - 1);
 	return (1);
 }
