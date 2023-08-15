166a356f1e52fc01acf8e7d3fd006117eb6ff783
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-15 12:59:04 -0700

    2nd advanced task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long int sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%ld\n", sum);
+}
