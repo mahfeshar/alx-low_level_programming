b43e8841f0589c44e3189399ebc20a091c134116
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-15 13:25:01 -0700

    4th advanced task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-	long int i, x1 = 1, x2 = 2, s = 3;
-	long long int sum = 3;
+	long int x1 = 1, x2 = 2, s = 3, sum = 3;
 
 	while (x2 <= 4000000)
 	{
 		s = x1 + x2;
-		sum += s;
+		if (s % 2 == 0)
+			sum += s;
 		x1 = x2;
 		x2 = s;
 	}
