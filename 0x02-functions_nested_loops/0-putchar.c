66f2bff896584f75daadb262e4380d14c3fe7e73
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-15 07:23:47 -0700

    edit 8 task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+        return (0);
+}
