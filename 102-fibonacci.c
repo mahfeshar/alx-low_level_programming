d817297b4cf80e25ae0c5d8d339a5927b6f047a7
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-15 13:12:36 -0700

    3rd advanced task

---
+++ 102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, x1 = 1, x2 = 2, sum;
+
+	printf("1, 2");
+	for (i = 0; i < 48; i++)
+	{
+		sum = x1 + x2;
+		printf(", %d", sum);
+		x1 = x2;
+		x2 = sum;
+	}
+	printf("\n");
+	return (0);
+}
