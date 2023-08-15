1dcef133eb8891f5c16552d3c19fe56345c5ef3b
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-15 14:59:07 -0700

    edit last task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int i;
 	unsigned long int x = 0, y = 1, sum;
-	unsigned long int x_1, x_2, y_1, y_2, s_1, s_2;
 
 	for (i = 0; i < 92; i++)
 	{
@@ -19,31 +18,6 @@ int main(void)
 
 		x = y;
 		y = sum;
-	}
-
-	x_1 = x / 10000000000;
-	x_2 = x % 10000000000;
-	y_1 = y / 10000000000;
-	y_2 = y % 10000000000;
-
-	for (; i < 98; i++)
-	{
-		s_1 = y_1 + x_1;
-		s_2 = y_2 + x_2;
-		if (y_2 + x_2 > 9999999999)
-		{
-			s_1 += 1;
-			s_2 %= 10000000000;
-		}
-
-		printf("%lu%lu", s_1, s_2);
-		if (i != 97)
-			printf(", ");
-
-		x_1 = y_1;
-		x_2 = y_2;
-		y_1 = s_1;
-		y_2 = s_2;
 	}
 	printf("\n");
 	return (0);
