9015788003fbcd3f7fdb6af70b2269b6bb2a2155
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-21 14:39:28 -0700

    C - Pointers, arrays and strings: 1st advanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 120;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}
