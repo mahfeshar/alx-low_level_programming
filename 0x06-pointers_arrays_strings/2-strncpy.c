2633fbd91b280dcb77dd8a3d2d0f4335a7493b3c
mahfeshar <mahmoudfeshar11@gmail.com>
2023-08-23 14:24:27 -0700

    C - More pointers, arrays and strings: All mandatory tasks

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	int len = strlen(src);
 
 	for (; i < n; i++)
 		dest[i] = src[i];
+
+	for (i = len; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
