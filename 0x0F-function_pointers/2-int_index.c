c1e21fd225837729b2b41880931043cc1d5bdaa7
mahfeshar <mahmoudfeshar11@gmail.com>
2023-09-13 03:37:03 -0700

    C - Function pointers: 2. To hell with circumstances;

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0 || array == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (; i < size; i++)
 	{
