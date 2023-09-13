702fbbb1586936be3bdb02587216a05bd03009df
mahfeshar <mahmoudfeshar11@gmail.com>
2023-09-13 04:58:23 -0700

    C - Function pointers: 3. A goal is not always meant to be reached,

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int __attribute__((__unused__)) argc, char *arg[])
+{
+	int a, b;
+	char *op;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	a = atoi(arg[1]);
+	b = atoi(arg[3]);
+	op = arg[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*op == '/' && b == 0) ||(*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the sum of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - the difference of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - the product of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - the division of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - the mod of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the mod of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
