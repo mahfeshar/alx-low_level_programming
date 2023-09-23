3e0fa6960ff1ce875bb43ad5c47ab2c7bb3e0048
mahfeshar <mahmoudfeshar11@gmail.com>
2023-09-23 14:47:05 +0000

    C - Recursion: 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * len - function to return length
+ * @s: string
+ * Return: length for string
+ */
+
+int len(char *s)
+{
+	int l = 0;
+	while (s[l])
+		l++;
+	return (l);
+}
+
+/**
+ * solve - solution for this problem
+ * @len: length 
+ * @i: first index in the string
+ * Return: 1 if the string is palindrome
+ */
+int solve(char *s, int len, int i)
+{
+	if (s[i] == s[len / 2])
+		return (1);
+
+	if (s[i] == s[len - i - 1])
+		return (solve(s, len, i + 1));
+	return (0);
+}
+
+/**
+* is_palindrome - functions check if string palindrome
+* @s: string
+* Return: if palindrome return 1 else return 0
+*/
+
+int is_palindrome(char *s)
+{
+	int i = 0, n = len(s);
+
+	if (*s == '\0')
+		return (1);
+	return (solve(s, n, i));
+}
