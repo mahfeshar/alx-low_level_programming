9aab2c4fe5398c4a98c0a9374f2a9887310503af
mahfeshar <mahmoudfeshar11@gmail.com>
2023-09-26 16:59:14 +0000

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - to print all list items
+ *
+ * @h: head for linked list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t cnt = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+My README file to linked list
