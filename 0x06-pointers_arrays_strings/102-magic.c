3b8eda36f3df867da83cb4ce0e0edb6a09c500d7
mahfeshar <mahmoudfeshar11@gmail.com>
2023-09-30 08:36:46 +0300

    7. Crackme3

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -15,9 +15,9 @@ int main(void)
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
-	*/
-
-	*(a + 2) = 98;
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
