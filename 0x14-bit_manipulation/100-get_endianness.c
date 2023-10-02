1d12ca4ada8b2c1855431aac1ef07921bc113462
mahfeshar <mahmoudfeshar11@gmail.com>
2023-10-03 02:11:22 +0300

    File I/O

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-* get_endianness - checks the endianness
-* Return: 0 if big endian, 1 if little endian
-*/
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
 int get_endianness(void)
-	{
+{
 	unsigned int i = 0x01020304;
 	unsigned char *p = (unsigned char *)&i;
 
