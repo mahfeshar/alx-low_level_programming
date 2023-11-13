4b1e8a35904fcff5f31798fb9339a2069094545c
mahfeshar <mahmoudfeshar11@gmail.com>
2023-11-13 12:58:06 +0200

    protection in main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,3 +23,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
